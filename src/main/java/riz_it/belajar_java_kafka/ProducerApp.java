25a8e40bb96d178e73bfa8ee05d4d8f4c6251045
riz.it <mrizalf.email@gmail.com>
2024-07-04 11:02:43 +0700

    Initial commit

---
+++ src/main/java/riz_it/belajar_java_kafka/ProducerApp.java
@@ -0,0 +1,25 @@
+package riz_it.belajar_java_kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerApp {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int i = 0; i < 10; i++) {
+            ProducerRecord<String, String> message = new ProducerRecord<>("helloworld", Integer.toString(i), "Hello " + i);
+            producer.send(message).get();
+        }
+    }
+}
---
+++ src/test/java/riz_it/belajar_java_kafka/BelajarJavaKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package riz_it.belajar_java_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
